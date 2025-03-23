16e0b7b3a12970cb5b24fe892941d59e50c07b63
EnkiEnki77 <layman212@gmail.com>
2025-03-23 01:11:33 -0400

    Finally learned what an API is.

---
+++ api_vs_sdk.py
@@ -0,0 +1,16 @@
+# API's allow one program to interface with another. Think about how Windows, Linux, and Mac offer
+# a GUI to allow you to interface with your computer without having to run bash commands for every single
+# thing you want to do. API's allow your software to interface with another piece of software without
+# having to implement the other software yourself. They abstract away implementation details of the
+# software being interfaced with.
+
+# The key word is interface. A tv remote is an interface to interact with your TV. The tv remote
+# only exposes the buttons, not the underlying circuitry and processes that actually control the tv.
+# Just as a server hosting a REST API only exposes the endpoints, not the underlying code that backs said
+# endpoints. API's allow your software to interface with certain features of another piece of software
+# without needing to care about the implementation details of said feature. Implementation details are
+# abstracted away.
+
+# A REST API is an API hosted on a web server that provides RESTful endpoints to interface with functionality
+# of the server.
+
---
+++ assistant_object.py
@@ -0,0 +1,1 @@
+# Object returned from the assistants.create SDK
